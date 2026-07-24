0b7a8a01d692ad279a5f089ac8801672f5b2030e
herberth <herbe@HXBERTH11>
2026-07-23 21:00:13 -0600

    Semana 2

---
+++ BibliotecaMVC/Controllers/LibrosController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BibliotecaMVC.Models;
+
+namespace BibliotecaMVC.Controllers
+{
+    public class LibrosController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Libro> Libros = new List<Libro>
+            {
+                new Libro { Id = 1, Titulo = "Cien Años de Soledad", Autor = "Gabriel García Márquez", Categoria = "Novela", Precio = 19.99m, Disponible = true },
+                new Libro { Id = 2, Titulo = "La casa de los espíritus", Autor = "Isabel Allende", Categoria = "Novela", Precio = 24.99m, Disponible = true },
+                new Libro { Id = 3, Titulo = "Ficciones", Autor = "Jorge Luis Borges", Categoria = "Cuento", Precio = 15.99m, Disponible = false },
+                new Libro { Id = 4, Titulo = "La ciudad y los perros", Autor = "Mario Vargas Llosa", Categoria = "Novela", Precio = 22.99m, Disponible = true },
+                new Libro { Id = 5, Titulo = "Rayuela", Autor = "Julio Cortázar", Categoria = "Novela", Precio = 20.99m, Disponible = false }
+            };
+            return View(Libros);
+        }
+    }
+}
---
+++ BibliotecaMVC/Models/Libro.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+namespace BibliotecaMVC.Models
+{
+    public class Libro
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public string Categoria { get; set; }
+        public decimal Precio { get; set; }
+        public bool Disponible { get; set; }
+    }
+}
