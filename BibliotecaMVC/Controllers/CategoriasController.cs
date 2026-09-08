1b814703be5b40c187cf621006043df30cc77be1
herberth <herbe@HXBERTH11>
2026-09-08 15:36:41 -0600

    se realizo la pracrica de la semana 8

---
+++ BibliotecaMVC/Controllers/CategoriasController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using BibliotecaMVC.Models;
+using Microsoft.Data.SqlClient;
+
+namespace BibliotecaMVC.Controllers
+
+{
+    public class CategoriasController : Controller
+    {
+        private readonly string _connectionString;
+        public CategoriasController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("BibliotecaDB");
+        }
+
+
+        public IActionResult Index()
+        {
+            var categorias = new List<Categoria>();
+
+            using (var conexion = new SqlConnection(_connectionString))
+            {
+                var sql = "SELECT Id, Nombre, Descripcion FROM Categorias";
+                using (var comando = new SqlCommand(sql, conexion))
+                {
+                    conexion.Open();
+                    using (var lector = comando.ExecuteReader())
+                    {
+                        while (lector.Read())
+                        {
+                            categorias.Add(new Categoria
+                            {
+                                Id = lector.GetInt32(0),
+                                Nombre = lector.GetString(1),
+                                Descripcion = lector.IsDBNull(2) ? null : lector.GetString(2)
+                            });
+                        }
+                    }
+                }
+            }
+
+
+            return View(categorias);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Create(Categoria categoria)
+        {
+            if (categoria == null || string.IsNullOrEmpty(categoria.Nombre))
+            {
+                ModelState.AddModelError("Nombre", "El nombre de la categoría es obligatorio.");
+                return View(categoria);
+            }
+
+            using (var conexion = new SqlConnection(_connectionString))
+            {
+                var sql = "INSERT INTO Categorias (Nombre, Descripcion) VALUES (@Nombre, @Descripcion)";
+
+                using (var comando = new SqlCommand(sql, conexion))
+                {
+                    comando.Parameters.AddWithValue("@Nombre", categoria.Nombre);
+                    comando.Parameters.AddWithValue("@Descripcion", (object)categoria.Descripcion ?? System.DBNull.Value);
+                    conexion.Open();
+                    comando.ExecuteNonQuery();
+                }
+            }
+
+            TempData["SuccessMessage"] = "Categoría creada exitosamente.";
+            return RedirectToAction("Index");
+
+
+        }
+
+    }
+}
